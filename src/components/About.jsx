cac3a849f3423625c4b867a9d22d16821fb1f8c7
Antim <antimsarker0@gmail.com>
2024-03-02 22:55:15 +0600

    education has added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import logo3 from '../assets/logo3.jpg'
+import logo2 from '../assets/logo2.jpeg'
 const About = () => {
   return (
     <div
@@ -12,24 +13,30 @@ const About = () => {
             About
           </p>
           <p className="text-xl mt-10">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-            deserunt illum mollitia officiis qui exercitationem perferendis neque
-            quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-            iste, totam fugiat recusandae rerum laborum perferendis molestiae
-            aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-            ab mollitia voluptatibus, a nostrum eveniet laborum!
+            
+          Hey there, I'm Antim, a Computer Science and Engineering graduate from IUBAT. I've got a knack for coding and love diving into new technologies. My projects reflect my passion for problem-solving and creativity. I'm always eager to learn and grow, and I'm ready to bring my skills to the table to
+           tackle any challenge that comes my way. Let's make some tech magic together!
           </p>
 
           <br />
-
-          <p className="text-xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-            pariatur, vel similique sint, nobis aspernatur ut praesentium
-            explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-            quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-            Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-            magni quo! Eum cupiditate debitis labore.
-          </p>
+<h2 className='text-xl font-bold inline border-b-4 border-gray-500 '>Education</h2>
+          <div>
+          <div className="flex items-center space-x-4 space-y-4">
+      <img src={logo3} alt="IUBAT Logo" className="h-16 w-16 mt-2 rounded-full transition duration-300 ease-in-out transform hover:scale-110" />
+      <div>
+        
+        <p className="text-gray-600">International University of Business Agriculture & Technology (BCSE)</p>
+      </div>
+    </div>
+    <div className="flex items-center space-x-4 space-y-4">
+      <img src={logo2} alt="IUBAT Logo" className="h-16 w-16 mt-2 rounded-full transition duration-300 
+      ease-in-out transform hover:scale-110" />
+      <div>
+        
+        <p className="text-gray-600">Abdul Kadir Mollah City College (HSC)</p>
+      </div>
+    </div>
+          </div>
         </div>
       </div>
     </div>
