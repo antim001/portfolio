03d2ca9492f35dd4c7f40fa362558d2ca271048d
Antim <antimsarker0@gmail.com>
2024-03-02 15:42:39 +0600

    contact update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,10 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+      className="w-full h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="mt-80 pb-8">
+        <div className="mt-40 pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
@@ -24,12 +24,12 @@ const Contact = () => {
               type="text"
               name="name"
               placeholder="Enter your name"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" required
             />
             <input
               type="text"
               name="email"
-              placeholder="Enter your email"
+              placeholder="Enter your email" required
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
